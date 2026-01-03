6035c4ec027d2a998608f010c981fef5e1b1d7b6
Titik-03 <titikmedgamal@gmail.com>
2026-01-03 16:19:08 +0100

    r

--- Python 01/ex6/ft_garden_analytics.py
+++ Python 01/ex6/ft_garden_analytics.py
@@ -40,6 +40,53 @@ class GardenManager:
             for p in plants:
                 score += p.height
                 if isinstance(p, PrizeFlower):
-                    p.points += 1
+                    score += p.point
             return score
-    
+    def __init__(self):
+        self.gardens = {}
+        GardenManager.total_manager += 1
+    def add_plant(self, garden_name, plant):
+            if garden_name not in self.gardens:
+                self.gardens[garden_name] = []
+            self.gardens[garden_name].append(plant)
+            print(f"Added {plant.name} to {garden_name}'s garden")
+    def grow_all(self, garden_name):
+            print(f"{garden_name} is helping all plants grow...")
+            if garden_name in self.gardens:
+                plants_to_grow = self.gardens[garden_name]
+                for p in plants_to_grow:
+                    p.grow()
+                else:
+                    print("Garden not found!")
+    @classmethod
+    def create_garden_manager(cls, capacity):
+        return cls
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    my_manager = GardenManager.create_garden_network(5)
+
+    p1 = Plant("Oak Tree", 100, 50)
+    p2 = FloweringPlant("Rose", 25, 30, "Red")
+    p3 = PrizeFlower("Sunflower", 80, 60, "Yellow", 10)
+    p4 = Plant("Cactus", 91, 10)
+
+    my_manager.add_plant("Alice", p1)
+    my_manager.add_plant("Alice", p2)
+    my_manager.add_plant("Alice", p3)
+    my_manager.add_plant("bob", p4)
+
+    my_manager.grow_all("Alice")
+
+    test_height = 15
+    is_valid = GardenManager.validate_height(test_height)
+    print(f"Height validation test ({test_height}cm): {is_valid}")
